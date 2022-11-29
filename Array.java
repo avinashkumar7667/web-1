e9e526bcfdbca96a70f5f2e6e2b6b8f848664332
avinashkumar7667 <115426329+avinashkumar7667@users.noreply.github.com>
2022-11-29 18:54:54 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 98;
+        marks[1] = 97;
+        marks[2] = 95;
+        for(int i=0; i<3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+    
+        
+    }
